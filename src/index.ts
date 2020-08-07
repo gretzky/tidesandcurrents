cd744ce3f4ddc200c63163cc062c865eafcf64ed
gretzky <sfed617@gmail.com>
2020-08-07 13:22:02 -0400

    units

--- src/index.ts
+++ src/index.ts
@@ -25,23 +25,13 @@ const get = async (
   stationId: number | string,
   params: Params
 ): Promise<AxiosResponse> => {
-  const localUsesImperial = await axios
-    .get("http://ipinfo.io")
-    .then(
-      res =>
-        res.data.country === "US" ||
-        res.data.country === "MM" ||
-        res.data.country === "LR"
-    );
-
-  const { format, time_zone, units, ...rest } = params;
+  const { format, time_zone, ...rest } = params;
 
   return axios.get("https://tidesandcurrents.noaa.gov/api/datagetter", {
     params: {
       station: stationId,
       format: format ?? "json",
       time_zone: time_zone ?? TimeZones.LDT,
-      units: units ?? localUsesImperial ? Units.IMPERIAL : Units.METRIC,
       ...rest,
     },
   });
@@ -55,13 +45,15 @@ const get = async (
  */
 const tidePredictions = (
   stationId: number | string,
-  date?: string
+  date?: string,
+  units?: Units.IMPERIAL | Units.METRIC
 ): Promise<ReturnData> => {
   return get(stationId, {
     product: Products.TIDE_PREDICTIONS,
     date: date ?? "today",
     datum: Datum.MLLW,
     interval: "hilo",
+    units: units ?? Units.IMPERIAL
   }).then(res => {
     if (!res || !res.data) {
       throw new Error("Something went wrong.");
@@ -108,13 +100,20 @@ const stationMetadata = (
     });
 };
 
+/**
+ * currentWaterLevel - get the current water level (as average lowest tide recorded) for a given station
+ * 
+ * @param stationId - ID of a station to get data for (station IDs can be found here: https://tidesandcurrents.noaa.gov/stations.html)
+ */
 const currentWaterLevel = (
-  stationId: number | string
+  stationId: number | string,
+  units?: Units.IMPERIAL | Units.METRIC
 ): Promise<ReturnData> => {
   return get(stationId, {
     date: 'latest',
     datum: 'MLLW',
-    product: 'water_level'
+    product: 'water_level',
+    units: units ?? Units.IMPERIAL
   }).then(res => res.data.data[0])
 }
 
